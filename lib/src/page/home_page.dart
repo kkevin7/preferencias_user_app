f7b8033b9e1723a53cb6b9b7926f1eaaa97ed055
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-06-07 12:17:33 -0600

    Adding Drawer Menu

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,26 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:preferencias_user_app/src/page/setting_page.dart';
 
 class HomePage extends StatelessWidget {
-
   static final String routeName = 'home';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Preferencias de Usuario'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          title: Text('Preferencias de Usuario'),
+        ),
+        drawer: _crearMenu(context),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Color secundario:'),
+            Divider(),
+            Text('Género:'),
+            Divider(),
+            Text('Nombre de usuario:'),
+            Divider(),
+          ],
+        ));
+  }
+
+  Drawer _crearMenu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
         children: <Widget>[
-          Text('Color secundario:'),
-          Divider(),
-          Text('Género:'),
-          Divider(),
-          Text('Nombre de usuario:'),
-          Divider(),
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/menu-img.jpg'),
+                    fit: BoxFit.cover)),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.pages,
+              color: Colors.blue,
+            ),
+            title: Text('Pages'),
+            onTap: () =>
+                Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.party_mode,
+              color: Colors.blue,
+            ),
+            title: Text('Party Mode'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.people,
+              color: Colors.blue,
+            ),
+            title: Text('People'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              color: Colors.blue,
+            ),
+            title: Text('Setting'),
+            onTap: () =>
+                Navigator.pushReplacementNamed(context, SettingPage.routeName),
+          ),
         ],
-      )
+      ),
     );
   }
 }
